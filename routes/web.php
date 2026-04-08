9c504255b05cf952a25eedcba2a502414d36459c
divanihutagaol31-droid <divanihutagaol31@gmail.com>
2026-04-08 20:23:07 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,26 @@ Route::prefix('management')->group(function () {
     Route::get('/karyawan', function () {
         return view('management.karyawan');
     })->name('management.karyawan');
-});
 
-Route::get('/absensi', function () {
-    return view('Karyawan.absensi');
-});
+    Route::get('/approval', function () {
+        return view('management.approval');
+        })->name('management.approval');
+    });
+Route::prefix('karyawan')->group(function () {
+    Route::get('/absensi', function () {
+        return view('karyawan.absensi');
+        })->name('karyawan.absensi');
 
-Route::get('/riwayat', function () {
-    return view('Karyawan.riwayat');
-});
+    Route::get('/laporan', function () {
+        return view('karyawan.laporan');
+        })->name('karyawan.laporan');
 
+    Route::get('/pengajuan', function () {
+        return view('karyawan.pengajuan');
+        })->name('karyawan.pengajuan');
 
-Route::get('/laporan', function () {
-    return view('Karyawan.laporan');
-});
+    Route::get('/riwayat', function () {
+        return view('karyawan.riwayat');
+        })->name('karyawan.riwayat');
 
-Route::get('/pengajuan', function () {
-    return view('Karyawan.pengajuan');
-});
+    });
