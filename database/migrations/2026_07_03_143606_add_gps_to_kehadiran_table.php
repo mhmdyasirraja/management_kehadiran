3f90238a21ecb48942af59aabb5148be03f27130
divanihutagaol31-droid <divanihutagaol31@gmail.com>
2026-07-06 19:47:48 +0700

    perbaikan error validasi lokasi

--- database/migrations/2026_07_03_143606_add_gps_to_kehadiran_table.php
+++ database/migrations/2026_07_03_143606_add_gps_to_kehadiran_table.php
@@ -1,6 +1,5 @@
 <?php
 
-// database/migrations/xxxx_xx_xx_add_gps_to_kehadirans_table.php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -9,7 +8,7 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::table('kehadirans', function (Blueprint $table) {
+        Schema::table('kehadiran', function (Blueprint $table) {
             $table->decimal('latitude_masuk', 10, 7)->nullable()->after('jam_masuk');
             $table->decimal('longitude_masuk', 10, 7)->nullable()->after('latitude_masuk');
             $table->decimal('latitude_keluar', 10, 7)->nullable()->after('jam_keluar');
@@ -19,7 +18,7 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::table('kehadirans', function (Blueprint $table) {
+        Schema::table('kehadiran', function (Blueprint $table) {
             $table->dropColumn(['latitude_masuk', 'longitude_masuk', 'latitude_keluar', 'longitude_keluar']);
         });
     }
